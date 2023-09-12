1dfb613f5aba2082a7032a86c37c854874d6d020
Sajjad-scbas <scbas1000@gmail.com>
2023-09-12 17:54:44 +0200

    add models

--- scripts/swin_tf_script.py
+++ scripts/swin_tf_script.py
@@ -1,10 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pickle
 
 import torch
 import torch.nn as nn
 
-from model.model import SwinTransformer
+from model.model_V2 import SwinTransformer
 from data.dataset import CustomDataset
 
 from sklearn.model_selection import train_test_split
@@ -12,12 +13,12 @@ from sklearn.model_selection import train_test_split
 from torch.utils.tensorboard import SummaryWriter
 from train.train import train_loop, test_loop
 
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, WeightedRandomSampler
 
 
 
 if __name__ == "__main__":
-    file_path = '/Users/scbas/Downloads/IdemiaExerciceML/data'
+    file_path = '/home/sajjad/Desktop/Face_Classification/data'
     labels = 'label_train.txt'
 
 
@@ -28,19 +29,22 @@ if __name__ == "__main__":
 
     with open(f"{file_path}/db_train.raw", 'rb') as file:
         train_data = file.read()
-    labels = np.loadtxt(f"{file_path}/label_train.txt", dtype = np.float32)
+    labels = np.loadtxt(f"{file_path}/label_train.txt", dtype=np.int8)
 
 
     # Assuming the data represents a grayscale image
     data = np.frombuffer(train_data, dtype=np.uint8).reshape(nb_images, height, width, channels).astype(np.float32)
     
-    train_data, val_data, train_lab, val_lab = train_test_split(data, labels, test_size=10130/111430) 
+
+    class_sample_count = np.unique(labels, return_counts=True)[1]
+
+    train_data, val_data, train_lab, val_lab = train_test_split(data, labels, stratify=labels, test_size=10130/111430) 
     
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     dtype = torch.float32   
     
     parameters = {
-        'kernel_size' : (4, 4),
+        'kernel_size' : (2, 2),
         'hidden_emb' : 48, 
         'nb_classes' : 2, 
         'nb_channels' : 3, 
@@ -48,38 +52,48 @@ if __name__ == "__main__":
         'norm_layer' : nn.LayerNorm,
         'loss' : nn.BCELoss(),
 
-        'num_layers' : 2,
+        'num_layers' : 4,
         'num_heads' : 3,
         'head_dim' : 16, 
-        'window_size' : (4, 4),
+        'window_size' : (2, 2),
         'relative_pos_embedding'  : True,
-        'attn_drop_rate' : 0.2,
-        'proj_drop_rate' : 0.2, 
+        'attn_drop_rate' : 0.1,
+        'proj_drop_rate' : 0.1, 
         'mlp_dim' : 48 * 2,
-        'batch_size' : 128,
+        'batch_size' : 64,
         'device' : device,
-        'lr': 5e-6,
-        'weight_decay' : 3e-7,
+        'lr': 1e-5,
+        'weight_decay' : 1e-6,
         'nb_epochs': 10,
+        'enc_layers': 3
     }
     
+
+
+    weight = 1. / class_sample_count
+    samples_weight = torch.from_numpy(weight[train_lab])
+    sampler = WeightedRandomSampler(samples_weight, 2*len(samples_weight), replacement=True)
+
     train_dataset = CustomDataset(data = train_data, labels = train_lab)
-    train_dataloader = DataLoader(train_dataset, batch_size=parameters['batch_size'], shuffle=True)
+    train_dataloader = DataLoader(train_dataset, batch_size=parameters['batch_size'], sampler=sampler)
+
 
     val_dataset = CustomDataset(data = val_data, labels = val_lab)
     val_dataloader = DataLoader(val_dataset, batch_size=parameters['batch_size'], shuffle=True)
     
     
-    writer = SummaryWriter(f"runs/modele0 == batch_size = {parameters['batch_size']}, kernel_size = {parameters['kernel_size']}, \
+    writer = SummaryWriter(f"runs/modele4 == batch_size = {parameters['batch_size']}, kernel_size = {parameters['kernel_size']}, \
         lr = {parameters['lr']}, window_size = {parameters['window_size']}, epochs = {parameters['nb_epochs']}")
     
     
+    #save model parameters
+    with open(f"model_N4_params.pkl", "wb") as file:
+        pickle.dump(parameters, file)
 
     
-    
     model = SwinTransformer(parameters).to(dtype = dtype, device= device)
-    
-    optimizer = torch.optim.Adam(model.parameters(), lr=parameters['lr'])
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=parameters['lr'], weight_decay=parameters['weight_decay'])
     criterion = nn.CrossEntropyLoss()
     
     nb_epochs = parameters['nb_epochs']
@@ -87,11 +101,11 @@ if __name__ == "__main__":
     
     for epoch in range(nb_epochs):
         print(f"Epoch {epoch+1}\n-------------------------------")
-        train_loop(train_dataloader, model, criterion, optimizer, epoch, writer)
-        test_loop(val_dataloader, model, criterion, epoch, writer)
+        train_loop(train_dataloader, model, criterion, optimizer, epoch, writer, device)
+        test_loop(val_dataloader, model, criterion, epoch, writer, device)
     
-        #save the model weights
-        torch.save(model.state_dict(), f"model0.pt")
+    #save the model weights
+    torch.save(model.state_dict(), f"model_N4.pt")
     
     print('Done')
     
--- train/train.py
+++ train/train.py
@@ -6,14 +6,14 @@ import torch.nn as nn
 from tqdm import tqdm
 
 
-def train_loop(dataloader, model, loss_fn, optimizer, epoch, writer):
+def train_loop(dataloader, model, loss_fn, optimizer, epoch, writer, device):
 
     nb_batchs = dataloader.batch_size
     model.train()
     for idx, (x, label) in enumerate(tqdm(dataloader)):
 
-        pred = model(x)
-        loss = loss_fn(pred, label.long())
+        pred = model(x.to(device))
+        loss = loss_fn(pred, label.long().to(device))
         
         #Back Pass
         loss.backward()
@@ -24,27 +24,30 @@ def train_loop(dataloader, model, loss_fn, optimizer, epoch, writer):
         optimizer.step()
         optimizer.zero_grad()
         
-        
+        acc = ((predicted == label.to(device)).sum())/nb_batchs
+
         writer.add_scalar('training_loss', loss.item(), (epoch*len(dataloader)) + idx)
         print(f'training-loss : {loss.item():>7f} | [{(idx+1)}/ {len(dataloader)}]')
-        print(f'Accuracy : {((predicted == label).sum())/nb_batchs:>7f} | [{(idx+1)}/ {len(dataloader)}]')
+        writer.add_scalar('Accuracy_train', acc, (epoch*len(dataloader)) + idx)
+        print(f'Accuracy : {acc:>7f} | [{(idx+1)}/ {len(dataloader)}]')
 
 
 
-def test_loop(dataloader, model, loss_fn, epoch, writer):
+def test_loop(dataloader, model, loss_fn, epoch, writer, device):
     test_loss = 0
     correct = 0
     nb_batchs = dataloader.batch_size
     model.eval()
     with torch.no_grad():
         for idx, (x, label) in enumerate(tqdm(dataloader)):
-            pred = model(x)
-            test_loss += loss_fn(pred, label.long()).item()
+            pred = model(x.to(device))
+            test_loss += loss_fn(pred, label.long().to(device)).item()
             
             _, predicted = torch.max(pred, 1)
             
-            correct += (predicted==label).sum()/nb_batchs
+            correct += (predicted==label.to(device)).sum()/nb_batchs
             
         test_loss /= len(dataloader)
         writer.add_scalar('testing_loss', test_loss, epoch)
-        print(f'Test MSE loss : {test_loss:>7f}, Accuracy : {correct:>7f}')
+        writer.add_scalar('Accuracy_test', correct/len(dataloader), epoch)
+        print(f'Test MSE loss : {test_loss:>7f}, Accuracy : {correct/len(dataloader):>7f}')
